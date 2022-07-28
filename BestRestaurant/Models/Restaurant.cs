c439c3af529ecfb2a3fd7b2802cdc7a2bc3a2625
Claire Thorington <claire.thorington@gmail.com>
2022-07-28 14:50:59 -0700

    after lunch commit

--- BestRestaurant/Models/Restaurant.cs
+++ BestRestaurant/Models/Restaurant.cs
@@ -5,6 +5,7 @@ namespace BestRestaurant.Models
     public int RestaurantId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    public string Location { get; set; }
     public int CuisineId { get; set; }
     public virtual Cuisine Cuisine { get; set; }
   }
